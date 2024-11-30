55f8802e2f4c8b9e3d3ab15cc2c4354a0b5b002b
1cedrus <aowiej23s@gmail.com>
2024-11-30 15:22:26 +0700

    Refactor 😝

--- main.c
+++ main.c
@@ -1,108 +1,166 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
 
 #include "include/raylib.h"
 #include "include/sewer.h"
+#include <math.h>
 
-#define WINDOW_WIDTH 400
+#define WINDOW_WIDTH 500
 #define WINDOW_HEIGHT 600
+#define DEATH_POSITION 550
+
+int freeSewer(Sewer* sewer);
+int freePump(Sewer** pump, int *size);
 
 int main() {
     InitAudioDevice();
+    SetMasterVolume(0.1);
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Raybird"); 
     SetTargetFPS(60);     
-    Vector2 p = {100, 300};
+
+    Sound wingSound = LoadSound("audio/wing.wav");
+    Sound pointSound = LoadSound("audio/point.wav");
+    Sound dieSound = LoadSound("audio/die.wav");
+    Sound hitSewerSound = LoadSound("audio/hit.wav");
+    Sound swooshSound = LoadSound("audio/swoosh.wav");
+
+    Vector2 position = {100, 300};
     Vector2 size = {50, 50};
+
     float speed = 0;
-    float a = -800;
-    int point = 0;
-    char pointText[10];
+    float acceleration = -800;
 
-    Sound wing = LoadSound("audio/wing.wav");
-    Sound hit = LoadSound("audio/point.wav");
+    uint16_t point = 0;
+    char pointStr[6];
 
-    Sewer* pump[10] = {0};
+    Sewer* pump[5] = {0};
     int pumpSize = 0;
 
-    int isCount = 0;
+    bool isStart = false;
+    bool alreadyCount = false;
+    bool isDie = false;
 
     while (!WindowShouldClose()) {
         float dt = GetFrameTime();
         BeginDrawing();
         ClearBackground(BLACK);
-        speed -= a * dt;   
 
-        if (IsKeyPressed(KEY_SPACE)) {
-            PlaySound(wing);
-            speed = -250;
-        }
-
-        p.y += speed * dt;
-        
-        Rectangle rec = {
-            p.x, p.y, size.x, size.y
-        };
-
-        DrawRectangleRec(rec, RED);
-        
-        if (!pumpSize || pump[pumpSize - 1]->xPosition < 800 - DEFAULT_SEWER_WIDTH * 2.2) {
-            Sewer* sewer = randomSewer();
-            pump[pumpSize++] = sewer;
-        } 
-
-        if (pumpSize && pump[0]->xPosition <= 0 && !isCount) {
-            point += 1;
-            PlaySound(hit);
-
-            isCount = 1;
-        }
-
-        if (pumpSize && pump[0]->xPosition < -DEFAULT_SEWER_WIDTH) {
-            free(pump[0]->upSewer);
-            free(pump[0]->downSewer);
-            free(pump[0]);
-
-            for (int i = 0; i < pumpSize - 1; i++) {
-                pump[i] = pump[i + 1];
+        if (!isStart) {
+            if (IsKeyPressed(KEY_SPACE)) {
+                isStart = true;
             }
 
-            pumpSize -= 1;
-            isCount = 0;
-        }
+            DrawText("Press space to start!", 75, 300, 32, WHITE);
+        } else {
+            speed -= acceleration * dt;   
 
-        for (int i = 0; i < pumpSize; i += 1) {
-            Rectangle sewerUp = {
-                pump[i]->xPosition, 0, pump[i]->upSewer->x, pump[i]->upSewer->y
+            position.y += speed * dt;
+            position.y = fminf(position.y, DEATH_POSITION);
+
+            if (position.y == DEATH_POSITION) {
+                isDie = true;
+            }
+
+            Rectangle birdRec = {
+                position.x, position.y, size.x, size.y
             };
 
-            DrawRectangleRec(sewerUp, GREEN);
+            if (isDie) {
+                if (position.y == DEATH_POSITION) {
+                    if (IsKeyPressed(KEY_SPACE)) {
+                        PlaySound(swooshSound);
 
-            Vector2 positionDown = {
-                pump[i]->xPosition, 600 - pump[i]->downSewer->y
-            };
+                        freePump(pump, &pumpSize);
 
-            Rectangle sewerDown = {
-                pump[i]->xPosition, 600 - pump[i]->downSewer->y,pump[i]->downSewer->x, pump[i]->downSewer->y
-            };
+                        speed = -250;
+                        point = 0;
+                        position.y = 300;
 
-            DrawRectangleRec(sewerDown, GREEN);
-
-            pump[i]->xPosition -= 150 * dt;
-
-            if (CheckCollisionRecs(rec, sewerUp) || CheckCollisionRecs(rec, sewerDown)) {
-                for (int i = 0; i < pumpSize - 1; i++) {
-                    free(pump[i]->upSewer);
-                    free(pump[i]->downSewer);
-                    free(pump[i]);
+                        isDie = false;
+                    }
                 }
 
-                point = 0;
-                pumpSize = 0;
-            }
-        }
+                for (int i = 0; i < pumpSize; i += 1) {
+                    Rectangle sewerUp = {
+                        pump[i]->xPosition, 0, pump[i]->upSewer->x, pump[i]->upSewer->y
+                    };
 
-        sprintf(pointText, "%d", point);
-        DrawText(pointText, 25, 25, 32, WHITE);
+                    DrawRectangleRec(sewerUp, GREEN);
+
+                    Vector2 positionDown = {
+                        pump[i]->xPosition, 600 - pump[i]->downSewer->y
+                    };
+
+                    Rectangle sewerDown = {
+                        pump[i]->xPosition, 600 - pump[i]->downSewer->y,pump[i]->downSewer->x, pump[i]->downSewer->y
+                    };
+
+                    DrawRectangleRec(sewerDown, GREEN);
+                }
+            } else {
+                if (IsKeyPressed(KEY_SPACE)) {
+                    PlaySound(wingSound);
+                    speed = -250;
+                }
+
+                if (!pumpSize || pump[pumpSize - 1]->xPosition < 800 - DEFAULT_SEWER_WIDTH * 2.2) {
+                    pump[pumpSize++] = randomSewer();
+                } 
+
+                if (pumpSize && pump[0]->xPosition <= 50 && !alreadyCount) {
+                    PlaySound(pointSound);
+                    point += 1;
+                    alreadyCount = true;
+                }
+
+                if (pumpSize && pump[0]->xPosition < -DEFAULT_SEWER_WIDTH) {
+                    freeSewer(pump[0]);
+
+                    // Shift the array to the left
+                    for (int i = 0; i < pumpSize - 1; i++) {
+                        pump[i] = pump[i + 1];
+                    }
+                    pumpSize -= 1;
+
+                    alreadyCount = false;
+                }
+
+                for (int i = 0; i < pumpSize; i += 1) {
+                    Rectangle sewerUp = {
+                        pump[i]->xPosition, 0, pump[i]->upSewer->x, pump[i]->upSewer->y
+                    };
+
+                    DrawRectangleRec(sewerUp, GREEN);
+
+                    Vector2 positionDown = {
+                        pump[i]->xPosition, 600 - pump[i]->downSewer->y
+                    };
+
+                    Rectangle sewerDown = {
+                        pump[i]->xPosition, 600 - pump[i]->downSewer->y,pump[i]->downSewer->x, pump[i]->downSewer->y
+                    };
+
+                    DrawRectangleRec(sewerDown, GREEN);
+
+                    pump[i]->xPosition -= 150 * dt;
+
+                    if (CheckCollisionRecs(birdRec, sewerUp) || CheckCollisionRecs(birdRec, sewerDown)) {
+                        isDie = 1;
+
+                        PlaySound(hitSewerSound); 
+                        PlaySound(dieSound);
+                    }
+                }
+
+                sprintf(pointStr, "%d", point);
+                DrawText(pointStr, 25, 25, 32, WHITE);
+            }
+
+            DrawRectangleRec(birdRec, RED);
+        }
 
         EndDrawing();
     }
@@ -111,5 +169,16 @@ int main() {
     return 0;
 }
 
+int freeSewer(Sewer* sewer) {
+    assert(sewer != NULL);
+    free(sewer->upSewer);
+    free(sewer->downSewer);
+    free(sewer);
+    return 1;
+}
 
-
+int freePump(Sewer** pump, int *size) {
+    assert(pump != NULL && size != NULL);
+    while ((*size) && freeSewer(pump[--(*size)])) {}
+    return 1;
+}
